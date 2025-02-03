b38a195db9c4420a5dc7b285c81aecec2b5be460
Maria Alice Aristides Silva <65632852+mariaaliceas@users.noreply.github.com>
2025-02-03 19:56:36 +0000

    feat: add styles and get post by slug

--- code-connect/src/app/posts/[slug]/page.tsx
+++ code-connect/src/app/posts/[slug]/page.tsx
@@ -1,7 +1,11 @@
-import styles from "./page.module.css";
 import { CardPost } from "@/components/CardPost";
+import styles from "./page.module.css";
 
-async function getPostsBySlug(slug: any) {
+interface Params {
+  slug: string;
+}
+
+async function getPostBySlug(slug: any) {
   const url = `http://localhost:3042/posts?slug=${slug}`;
   const response = await fetch(url);
 
@@ -9,5 +13,21 @@ async function getPostsBySlug(slug: any) {
   if (data.length === 0) {
     return {};
   }
-  return data[0];
+  const post = data[0];
+  return post;
 }
+
+const PagePost = async ({ params }: { params: Params }) => {
+  const post = await getPostBySlug(params.slug);
+  return (
+    <div>
+      <CardPost post={post} />
+      <h3 className={styles.subtitle}>Código:</h3>
+      <div className={styles.code}>
+        <div dangerouslySetInnerHTML={{ __html: post.markdown }} />
+      </div>
+    </div>
+  );
+};
+
+export default PagePost;
